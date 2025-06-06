43f1e0988eb4c19ed48568e5fa083c5cc17ee2ee
AndreyJuruns <ffakenatty01@gmail.com>
2025-06-06 12:43:56 -0400

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -66,7 +66,7 @@ else:
     # ----- SIDEBAR -----
     st.sidebar.image(logo_path, width=1000)
     st.sidebar.markdown(
-        "👨‍🔬 **Researchers:**  \nAndrey Gaspar Sorrilha Rodrigues  \nProfº Dra. Aline Regina Hellmann Carollo  \nProfº Dr. Carlos Alexandre Carollo  \nDra. Denise Brentan da Silva")
+        "👨‍🔬 **Researchers:**  \nAndrey Gaspar Sorrilha Rodrigues  \nProfº Dra. Aline Regina Hellmann Carollo  \nProfº Dra. Denise Brentan da Silva  \nProfº Dr. Carlos Alexandre Carollo")
     st.sidebar.title("🔬 Bioactive Compounds Analysis")
     st.sidebar.markdown("---")  # Line divisória
     st.sidebar.title("⚙️ Settings")
